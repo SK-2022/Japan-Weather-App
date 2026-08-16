94bc212af5d405ad743ed0fe0f158ce90e3b5999
Mika Chiusiwa <mikachiusiwa@gmail.com>
2026-08-17 08:58:47 +0900

    Refactor index.html structure and update getWeather function in script.js for improved readability and error handling

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// Functions that take a location and return the weather data for that location. 
-
+// Functions that take a location and return the weather data for that location.
+//Ensure to rewrite this code again using 1. regular promise syntax and 2. using the throw new error feature
 async function getWeather() {
-    let location = "Sendai";
-    let date1 = "Today";
-    let date2 = "Tomorrow";
-    try {
-        let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?key=NQ7MF6XN5T5GXL6XVR4XGKF9U`)
-        let weatherAppData = await response.json()
-        return weatherAppData
-    } 
+  let location = "Sendai";
+  try {
+    let response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=NQ7MF6XN5T5GXL6XVR4XGKF9U`,
+    );
+    let weatherAppData = await response.json();
+    return console.log(weatherAppData);
+  } catch (error) {
+    console.log("Something done got fucked up", error);
+  }
 }
+
+getWeather();
