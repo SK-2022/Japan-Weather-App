ca7dea6f1ea43f722e6858fd0796a73fecc7bf41
Mika Chiusiwa <mikachiusiwa@gmail.com>
2026-08-17 09:14:02 +0900

    Update input field classes and refactor getWeather function for dynamic location input

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 // Functions that take a location and return the weather data for that location.
 //Ensure to rewrite this code again using 1. regular promise syntax and 2. using the throw new error feature
-async function getWeather() {
-  let location = "Sendai";
+
+let searchButton = document.querySelector(".search-button");
+
+searchButton.addEventListener("click", async () => {
+  let locationInputField = document.querySelector(".location-input-field");
+  let locationInput = locationInputField.value;
+
   try {
     let response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=NQ7MF6XN5T5GXL6XVR4XGKF9U`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationInput}?key=NQ7MF6XN5T5GXL6XVR4XGKF9U`,
     );
+
     let weatherAppData = await response.json();
+
     return console.log(weatherAppData);
   } catch (error) {
-    console.log("Something done got fucked up", error);
+    console.log("Something broke", error);
   }
-}
-
-getWeather();
+});
